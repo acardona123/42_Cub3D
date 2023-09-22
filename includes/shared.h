1e9b4c0a4376e2caa4731d731d90eec87a30f0bb
Alex Cardona <42.alex.cardona@gmail.com>
2023-09-22 17:18:09 +0200

    general structures defined Defines all the structures requiered for the program execution (as far as we know for mow)
    	new file:   includes/mlx_settings.h
    	modified:   includes/shared.h
    	new file:   src/mlx_settings/mlx_setting_hooks.c
    	modified:   src/raycasting/ray_collision.c

--- includes/shared.h
+++ includes/shared.h
@@ -6,7 +6,7 @@
 /*   By: acardona <acardona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:44:59 by acardona          #+#    #+#             */
-/*   Updated: 2023/09/22 15:12:42 by acardona         ###   ########.fr       */
+/*   Updated: 2023/09/22 17:16:35 by acardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,6 @@
 # define SHARED_H
 
 # include "tools.h"
-
-
-// ==== Map datas ====
-
-typedef enum e_bloc_type
-{
-	FLOOR = '0',
-	DOOR = '2',
-	WALL = '1',
-	NOTHING = ' ',
-}	t_bloc_type;
-
-typedef struct s_data_map
-{
-	float	x_max;
-	float	y_max;
-	char	**map;
-}	t_data_map;
-
-// ---- End: Map data ----
-
 
 
 // ==== Textures ====
@@ -50,26 +29,88 @@ typedef struct s_data
 	int		endian;
 }	t_data;
 
-typedef struct s_static_texture_tail
+typedef struct s_static_texture
 {
 	char	*path;
 	int		img_width;
 	int		img_height;
 	t_data	data;
-}	t_static_texture_tail;
+}	t_static_texture;
 
-typedef struct s_animated_texture_tail
+typedef struct s_animated_textures
 {
-	unsigned int			frame_number;
-	unsigned int			frame_t0;//ref time for the animations
-	unsigned int			frame_ms;//time between 2 consecutive frames without pause
-	unsigned int			frame_pause_ms;
-	unsigned int			frame_cycle_short;//time to display all frames without pause
-	unsigned int			frame_cycle_long;//time of the complete cycle including pause
-	t_static_texture_tail	*frame_table;
-}	t_animated_texture_tail;
-
+	unsigned int		frame_number;
+	unsigned int		frame_ms;//time between 2 consecutive frames without pause
+	unsigned int		frame_pause_ms;
+	unsigned int		frame_cycle_short;//time to display all frames without pause
+	unsigned int		frame_cycle_long;//time of the complete cycle including pause
+	t_static_texture	*frame_table;
+}	t_animated_textures;
 
 // ---- End: Textures ----
+
+
+// ==== Map datas ====
+
+typedef enum e_chunk_type
+{
+	FLOOR = '0',
+	DOOR = '2',
+	WALL = '1',
+	NOTHING = ' ',
+}	t_chunk_type;
+
+typedef enum e_door_status
+{
+	DOOR_OPEN,
+	DOOR_OPENING,
+	DOOR_CLOSING,
+	DOOR_CLOSED
+}	t_door_status;
+
+typedef struct s_chunk
+{
+	t_chunk_type		type;
+	char				status;
+	unsigned int		t0;//ref time for the animations
+	t_animated_textures	*textures;
+}	t_chunk;
+
+typedef struct s_map
+{
+	float	x_max;
+	float	y_max;
+	t_chunk	**map;
+}	t_map;
+
+// ---- End: Map data ----
+
+
+
+// ==== Player ====
+
+typedef	struct s_player
+{
+	t_coord		p_co;
+	float		p_angle;
+	float		p_speed;
+}	t_player;
+
+// ---- End: Player ----
+
+typedef struct s_display
+{
+	void	*mlx;
+	void	*win;
+	t_data	*buff;
+}	t_display;
+
+typedef struct s_general
+{
+	t_map		map;
+	t_player	player;
+	t_display	disp;
+	float		fov;
+}	t_general;
 
 #endif
--- src/raycasting/ray_collision.c
+++ src/raycasting/ray_collision.c
@@ -6,7 +6,7 @@
 /*   By: acardona <acardona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 16:14:53 by acardona          #+#    #+#             */
-/*   Updated: 2023/09/22 15:20:09 by acardona         ###   ########.fr       */
+/*   Updated: 2023/09/22 17:01:27 by acardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ static void	_r_ray_init_v(t_coord *P, float angle_ray, t_coord *last_v,
 	}
 }
 
-static bool	_r_point_inside_map_borders(t_data_map *data, t_coord *P)
+static bool	_r_point_inside_map_borders(t_map *data, t_coord *P)
 {
 	if (P->x < 1 || P->y < 1 || P->x > data->x_max - 1 || P->y > data->y_max)
 		return (false);
@@ -64,7 +64,7 @@ static bool	_r_point_inside_map_borders(t_data_map *data, t_coord *P)
 }
 
 static t_coord	_r_ray_collision(t_bloc_type type, t_coord P, float angle_ray,
-	t_data_map *data)
+	t_map *data)
 {
 	t_coord		last_h;
 	t_coord		last_v;
@@ -335,7 +335,7 @@ int	main(int ac, char **av)
 		}
 	}
 
-	t_data_map	data = {10, 10, (char **)map_transposee};
+	t_map	data = {10, 10, (char **)map_transposee};
 	t_coord	co_p = {atof(av[1]), atof(av[2])};
 	
 	float	angle_ray = 0;
